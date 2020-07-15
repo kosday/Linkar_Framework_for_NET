0c00a02ae4c8570f72a361d4f226f874db0990f0
Tony Gravagno <tg@nebula-rnd.com>
2020-07-15 15:25:40 -0700

    Change LkDataSchProp to LkDataSchemaDetail. This could have been called LkDataSchemaProperties but there's already a RowProperties property in there and it could be confusing to have the word repeated so many times. It could also have been LkDataSchemaValues or Info ... doesn't matter.

---
+++ Linkar.LkData/LkDataSchemaDetail.cs
@@ -0,0 +1,21 @@
+﻿namespace Linkar.LkData
+{
+    /// <summary>
+    /// Class to manage the result of LkSchema and LkProperties operations.
+    /// </summary>
+    public class LkDataSchemaDetail : LkData
+    {
+        public string[] RowProperties { get; }
+        public string[] RowHeaders { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the LkDataSchemaDetail class.
+        /// </summary>
+        /// <param name="lkSchemaResult">The string result of the LkSchema or LkProperties operation execution.</param>
+        public LkDataSchemaDetail(string lkSchemaResult) : base(lkSchemaResult)
+        {
+            this.RowProperties = StringFunctions.ExtractRowProperties(lkSchemaResult);
+            this.RowHeaders = StringFunctions.ExtractRowHeaders(lkSchemaResult);
+        }
+    }
+}
